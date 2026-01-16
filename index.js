8403bf3ba1aa7fd855f45813ba391f39386494d6
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-01-17 04:03:12 +0700

    Include Bootstrap and create public folder

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static('public'));
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
