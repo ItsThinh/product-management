784902fab5d8258e06d248daabcb4055513f68dc
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-01-21 01:05:23 +0700

    Create product list on client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,20 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    const newProducts = products.map(item => {
+        const newPrice = Math.floor((item.price - item.discountPercentage / 100 * item.price) * 100) / 100;
+        item.newPrice = newPrice.toFixed(2);
+        return item;
+    });
+
     res.render("client/pages/products/index", {
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products: newProducts
     });
 }
