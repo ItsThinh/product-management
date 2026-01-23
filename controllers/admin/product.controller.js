30d6f04a9805eaf2a2a7418d87591ae12c636877
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-01-23 19:24:55 +0700

    feat(admin): add admin product page

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,15 @@
+const Product = require('../../models/product.model');
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
+    res.render('admin/pages/products/index', {
+        pageTitle: "Quản lý danh sách sản phẩm",
+        products: products
+    });
+}
