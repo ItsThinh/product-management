3f1c615246f9f22fcc9e2f45cfc8dc7a88531e61
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-01-26 05:13:27 +0700

    refactor(admin): improve filter code readability

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,24 +1,9 @@
 const Product = require('../../models/product.model');
+const filterStatusHelper = require('../../helpers/filterStatus');
 
 module.exports.index = async (req, res) => {
 
-    const filterButtons = [
-        {
-            name: "Tất cả",
-            status: '',
-            class: ''
-        },
-        {
-            name: "Hoạt động",
-            status: 'active',
-            class: ''
-        },
-        {
-            name: "Dừng hoạt động",
-            status: 'inactive',
-            class: ''
-        }
-    ];
+    const filterStatus = filterStatusHelper(req.query);
 
     const find = {
         deleted: false
@@ -26,12 +11,6 @@ module.exports.index = async (req, res) => {
 
     if (req.query.status) {
         find.status = req.query.status;
-
-        const index = filterButtons.findIndex(item => item.status === find.status);
-        filterButtons[index].class = 'active';
-    } else {
-        const index = filterButtons.findIndex(item => item.status === '');
-        filterButtons[index].class = 'active';
     }
 
     let keyword = '';
@@ -45,7 +24,7 @@ module.exports.index = async (req, res) => {
     res.render('admin/pages/products/index', {
         pageTitle: "Quản lý danh sách sản phẩm",
         products: products,
-        filterButtons: filterButtons,
+        filterStatus: filterStatus,
         keyword: keyword
     });
 }
