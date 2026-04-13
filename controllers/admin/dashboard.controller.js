0eb678eda5068c57eaf1741d5030ac72b79a90b6
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-04-13 15:35:27 +0700

    feat(admin): add dashboard detail information

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,43 @@
-module.exports.dashboard = (req, res) => {
+const ProductCategory = require('../../models/productCategory.model');
+const Product = require('../../models/product.model');
+const Account = require('../../models/account.model');
+const User = require('../../models/user.model');
+
+// [GET] /admin/dashboard
+module.exports.dashboard = async (req, res) => {
+
+    const getStatsForModel = async (Model) => {
+        const stats = await Model.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: 1},
+                    active: { $sum: { $cond: [{ $eq: ['$status', 'active'] }, 1, 0] } },
+                    inactive: { $sum: { $cond: [{ $eq: ['$status', 'inactive'] }, 1, 0] } },
+                    deleted: { $sum: { $cond: [{ $eq: ['$deleted', true] }, 1, 0] } }
+                }
+            }
+        ]);
+
+        return stats[0] || { total: 0, active: 0, inactive: 0, deleted: 0 };
+    };
+
+    const [productCategory, product, account, user] = await Promise.all([
+        getStatsForModel(ProductCategory),
+        getStatsForModel(Product),
+        getStatsForModel(Account),
+        getStatsForModel(User),
+    ]);
+
+    const statistics = {
+        productCategory,
+        product,
+        account,
+        user
+    }
+    
     res.render('admin/pages/dashboard/index', {
-        pageTitle: "Trang tổng quan"
+        pageTitle: "Trang tổng quan",
+        statistics: statistics
     });
 };
