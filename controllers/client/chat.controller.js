6c41525be8c3b65e714eccec363d1ba507b64e0e
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-04-23 11:37:15 +0700

    feat(client): implement real-time chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,19 +3,31 @@ const Chat = require('../../models/chat.model');
 // [GET] /chat
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
     
     // Socket.io
     _io.once('connection', (socket) => {
         socket.on('CLIENT_SEND_MESSAGE', async (content) => {
+            // Lưu vào database
             const chat = new Chat({
                 user_id: userId,
                 content: content
             });
             await chat.save();
+
+            // Trả data về client 
+            _io.emit('SERVER_RETURN_MESSAGE', {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            });
         });
+
     });
     // End Socket.io
+    
 
+    // Add User Information To Chat
     const chatsWithUserInfo = await Chat.aggregate([
         // Tìm các chat có deleted: false
         {
@@ -54,6 +66,8 @@ module.exports.index = async (req, res) => {
             }
         }
     ]);
+
+    // End Add User Information To Chat
 
     res.render('client/pages/chat/index', {
         pageTitle: 'Chat',
--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,3 +1,12 @@
+// Scroll Chat To Bottom
+const ScrollToBottom = () => {
+    const chatBody = document.querySelector('#chat-container');
+    chatBody.scrollTop = chatBody.scrollHeight;
+}
+
+ScrollToBottom();
+// End Scroll Chat To Bottom
+
 // CLIENT_SEND_MESSAGE
 const formSendDdata = document.querySelector('#input-area');
 if (formSendDdata) {
@@ -8,6 +17,32 @@ if (formSendDdata) {
             socket.emit('CLIENT_SEND_MESSAGE', content);
             e.target.elements.content.value = '';
         }
-    });    
+    });
+    ScrollToBottom();
 }
 // End CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on('SERVER_RETURN_MESSAGE', (data) => {
+    const userId = document.querySelector('[my-id]').getAttribute('my-id');
+    const div = document.createElement('div');
+    let htmlFullName = '';
+    if (userId == data.userId) {
+        div.classList.add('message', 'sent');
+    } else {
+        htmlFullName = `<strong>${data.fullName}:</strong>`;
+        div.classList.add('message', 'received');
+    }
+    div.innerHTML = `
+        ${htmlFullName}
+        <span>${data.content}</span>
+    `;
+
+    const chatBody = document.querySelector('#chat-container');
+    chatBody.appendChild(div);
+    ScrollToBottom();
+});
+// End SERVER_RETURN_MESSAGE
+
+
+
