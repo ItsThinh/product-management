0b27b08586865e4ee8a5fc634134ecbb02af754d
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-09-05 20:18:05 +0700

    feat(users): remove user card from not-friend list on incoming friend request

--- public/client/js/users.js
+++ public/client/js/users.js
@@ -54,12 +54,49 @@ if (badgeUserAccept) {
 }
 // End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
 
-// DELEGATION BUTTON EVENT
+// accept-requests.pug data-users-accept
 const dataUsersAccept = document.querySelector('[data-users-accept]');
 if (dataUsersAccept) {
-    // SERVER_RETURN_USER_INFO_ADD_FRIEND
-    const userId = dataUsersAccept.getAttribute('data-users-accept');
-    socket.on('SERVER_RETURN_INFO_ADD_FRIEND', (data) => {
+    // SERVER_RETURN_INFO_CANCEL_FRIEND
+    socket.on('SERVER_RETURN_USER_ID_CANCEL_FRIEND', (data) => {
+        const userId = dataUsersAccept.getAttribute('data-users-accept');
+        if (userId == data.userId) {
+            const boxCancelFriend = dataUsersAccept.querySelector(`[user-id='${data.idUserCancel}']`);
+            if (boxCancelFriend)
+                boxCancelFriend.remove();
+        }
+    })
+    // End SERVER_RETURN_INFO_CANCEL_FRIEND
+
+    // DELEGATION BUTTON EVENT
+    dataUsersAccept.addEventListener('click', (e) => {
+        // CLIENT_ACCEPT_FRIEND
+        const btnAccept = e.target.closest('[btn-accept-friend]');
+        if (btnAccept) {
+            btnAccept.closest('.box-user').classList.add('accepted');
+            const userId = btnAccept.getAttribute('btn-accept-friend');
+            socket.emit('CLIENT_ACCEPT_FRIEND', userId);
+        }
+        // End CLIENT_ACCEPT_FRIEND
+
+        // CLIENT_REFUSE_FRIEND
+        const btnRefuse = e.target.closest('[btn-refuse-friend]');
+        if (btnRefuse) {
+            btnRefuse.closest('.box-user').classList.add('refuse');
+            const userId = btnRefuse.getAttribute('btn-refuse-friend');
+            socket.emit('CLIENT_REFUSE_FRIEND', userId);
+        }
+        // End CLIENT_REFUSE_FRIEND
+    });
+    // End DELEGATION BUTTON EVENT
+}
+
+const dataUserNotFriend = document.querySelector('[data-users-not-friend]');
+// SERVER_RETURN_USER_INFO_ADD_FRIEND
+socket.on('SERVER_RETURN_INFO_ADD_FRIEND', (data) => {
+    // accept-requests.pug: Thêm box user khi họ kết bạn
+    if (dataUsersAccept) {
+        const userId = dataUsersAccept.getAttribute('data-users-accept');
         if (userId == data.userId) {
             const newBoxUser = document.createElement('div');
             newBoxUser.classList.add('col-6');
@@ -103,39 +140,18 @@ if (dataUsersAccept) {
 
             dataUsersAccept.appendChild(newBoxUser);
         }
-    });
-    // End SERVER_RETURN_USER_INFO_ADD_FRIEND
+    }
+    // End accept-requests.pug: Thêm box user khi họ kết bạn
 
-    // SERVER_RETURN_INFO_CANCEL_FRIEND
-    socket.on('SERVER_RETURN_USER_ID_CANCEL_FRIEND', (data) => {
+    // not-friend.pug: Xóa user đang hiển thị nếu được họ gửi kết bạn
+    if (dataUserNotFriend) {
+        const userId = dataUserNotFriend.getAttribute('data-users-not-friend');
         if (userId == data.userId) {
-            const boxCancelFriend = dataUsersAccept.querySelector(`[user-id='${data.idUserCancel}']`);
-            if (boxCancelFriend)
-                boxCancelFriend.remove();
-
+            const boxUser = dataUserNotFriend.querySelector(`[user-id='${data.infoUser._id}']`);
+            if (boxUser)
+                boxUser.remove();
         }
-    })
-    // End SERVER_RETURN_INFO_CANCEL_FRIEND
-
-    dataUsersAccept.addEventListener('click', (e) => {
-        // CLIENT_ACCEPT_FRIEND
-        const btnAccept = e.target.closest('[btn-accept-friend]');
-        if (btnAccept) {
-            btnAccept.closest('.box-user').classList.add('accepted');
-            const userId = btnAccept.getAttribute('btn-accept-friend');
-            socket.emit('CLIENT_ACCEPT_FRIEND', userId);
-        }
-        // End CLIENT_ACCEPT_FRIEND
-
-        // CLIENT_REFUSE_FRIEND
-        const btnRefuse = e.target.closest('[btn-refuse-friend]');
-        if (btnRefuse) {
-            btnRefuse.closest('.box-user').classList.add('refuse');
-            const userId = btnRefuse.getAttribute('btn-refuse-friend');
-            socket.emit('CLIENT_REFUSE_FRIEND', userId);
-        }
-        // End CLIENT_REFUSE_FRIEND
-    });
-}
-// End DELEGATION BUTTON EVENT
-
+    }
+    // End not-friend.pug: Xóa user đang hiển thị nếu được họ gửi kết bạn
+});
+// End SERVER_RETURN_USER_INFO_ADD_FRIEND
